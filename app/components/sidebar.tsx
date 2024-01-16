a3600e1533d0838285973a00a97d2eb3d2c1695c
Doraemon-code <51839881+Doraemon-code@users.noreply.github.com>
2024-01-16 10:43:06 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          Doraemon
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          小叮当的百宝袋.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
