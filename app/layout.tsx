744aab8992f84b400cb2f9b5ae74f1fdf8477336
Doraemon-code <51839881+Doraemon-code@users.noreply.github.com>
2024-08-18 13:19:05 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "小叮当的百宝袋",
+  description: "遇事不决，可问春风；春风不语，即随本心",
   appleWebApp: {
-    title: "NextChat",
+    title: "小叮当的百宝袋",
     statusBarStyle: "default",
   },
 };
